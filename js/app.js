1aaf47caa5b786e76c13e3e3d9c333b38e47c594
Ramii195 <ramiromartinez3596@gmail.com>
2025-09-07 13:49:21 -0300

    Ordene archivos y catalogo responsive

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // app.js  (usar <script type="module"> en tu HTML)
 import productos from './productos.js';
 
+
 const contenedor   = document.getElementById('catalogo-grid');
 const paginacion   = document.getElementById('paginacion');   
 const inputSearch  = document.getElementById('search');      
@@ -239,4 +240,29 @@ document.addEventListener("DOMContentLoaded", () => {
   const mq = window.matchMedia("(min-width: 751px)");
   const onChange = () => { if (mq.matches) closeMenu(); };
   mq.addEventListener ? mq.addEventListener("change", onChange) : mq.addListener(onChange);
+});
+
+
+
+// ================== Productos destacados ==========================
+
+const cont = document.getElementById("destacados");
+
+
+const destacados = productos.filter(p => p.destacado).slice(0, 4);
+
+// render súper simple: cada card es un <a> al detalle
+destacados.forEach(p => {
+  cont.innerHTML += `
+    <a class="product-card" href="./product.html?id=${encodeURIComponent(p.id)}">
+      <img src="${p.imagen}" alt="${p.nombre}" class="product-img">
+      <div class="product-info">
+        <h3 class="product-title">${p.nombre}</h3>
+        <div class="informa-img">
+          <p class="product-description">${p.descripcion ?? ""}</p>
+          <p class="product-price">$${Number(p.precio).toLocaleString("es-AR")}</p>
+        </div>
+      </div>
+    </a>
+  `;
 });
